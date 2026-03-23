c2379263a06723e18caeeeac241c78199c444bda
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-03-23 13:49:19 +0100

    add styles amount

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import './styles/app.scss'
 
 function App() {
   let x = 0
@@ -8,91 +9,96 @@ function App() {
   const [total, setTotal] = useState(0)
   const [custom, setCustom] = useState(0)
 
-  function handleClick(pourcent){
-    let result = (bill * pourcent)/100
+  function handleClick(pourcent) {
+    let result = (bill * pourcent) / 100
     setAmount(result / people)
     setTotal((parseFloat(bill) + result) / 5)
 
-    if (custom){
+    if (custom) {
       pourcent = custom
     }
   }
 
-  function reset(){
+  function reset() {
     setBill(0)
     setPeople(0)
     setAmount(0)
     setTotal(0)
   }
-  
+
   return (
     <>
-    <h1>Spliter</h1>
+      <section>
 
-    <div>
-      
-      <h2>Bill</h2>
-      <input 
-      type="number" 
-      value={bill} 
-      onChange={(e) => {setBill(e.target.value)}} />
+        <img src="images\logo.svg" alt="logo" />
 
-      <div>
-        <h2>Select tip %</h2>
-        <article>
-          <button onClick={() => handleClick(5)}>5%</button>
-        </article>
-        <article>
-          <button onClick={() => handleClick(10)}>10%</button>
-        </article>
-        <article>
-          <button onClick={() => handleClick(15)}>15%</button>
-        </article>
-        <article>
-          <button onClick={() => handleClick(25)}>25%</button>
-        </article>
-        <article>
-          <button onClick={() => handleClick(50)}>50%</button>
-        </article>
-        <article>
-          <button onClick={() => handleClick(custom)}>
-            <input type="text" 
-            placeholder="Custom"
-            value={custom}
-            onChange={(e) => {setCustom(e.target.value)}} />
-          </button>
-        </article>
-        <article>
-          
-        </article>
-      </div>
+        <div className="container">
 
-      <h2>Number of People</h2>
-      <input
-      type="number" 
-      value={people} 
-      onChange={(e) => {setPeople(e.target.value)}} />
+          <div className="bill">
+            <h2>Bill</h2>
+            <input
+              type="number"
+              value={bill}
+              onChange={(e) => { setBill(e.target.value) }}
+            />
 
-      <article>
-        <div>
-          <h3>Tip Amount</h3>
-          <p>/ person</p>
+            <h2>Select tip %</h2>
+            <article>
+              <button onClick={() => handleClick(5)}>5%</button>
+            </article>
+            <article>
+              <button onClick={() => handleClick(10)}>10%</button>
+            </article>
+            <article>
+              <button onClick={() => handleClick(15)}>15%</button>
+            </article>
+            <article>
+              <button onClick={() => handleClick(25)}>25%</button>
+            </article>
+            <article>
+              <button onClick={() => handleClick(50)}>50%</button>
+            </article>
+            <article>
+              <button onClick={() => handleClick(custom)}>
+                <input type="text"
+                  placeholder="Custom"
+                  value={custom}
+                  onChange={(e) => { setCustom(e.target.value) }} />
+              </button>
+            </article>
+
+            <h2>Number of People</h2>
+            <input
+              type="number"
+              value={people}
+              onChange={(e) => { setPeople(e.target.value) }}
+            />
+          </div>
+
+          <div className="amount">
+            <article>
+              <div>
+                <h3>Tip Amount</h3>
+                <p>/ person</p>
+              </div>
+              <h2>${amount.toFixed(2)}</h2>
+            </article>
+
+            <article>
+              <div>
+                <h3>Total</h3>
+                <p>/ person</p>
+              </div>
+              <h2>${total.toFixed(2)}</h2>
+            </article>
+
+            <button onClick={() => reset()}>Reset</button>
+          </div>
         </div>
-        <h2>${amount.toFixed(2)}</h2>
-      </article>
 
-      <article>
-        <div>
-          <h3>Total</h3>
-          <p>/ person</p>
-        </div>
-        <h2>${total.toFixed(2)}</h2>
-      </article>
-
-      <button onClick={() => reset()}>Reset</button>
-    </div>
+      </section>
     </>
-    
+
   )
 }
 

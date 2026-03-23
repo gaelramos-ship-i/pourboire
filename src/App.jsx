1d56a1ea504ac8b910fb0d665f0b9e3ffc46318a
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-03-23 10:20:42 +0100

    add tip amount

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react"
 
 function App() {
+  const [bill, setBill] = useState(0)
+  const [people, setPeople] = useState(0)
+
+  function handleClick(pourcent){
+    let result = (bill * pourcent)/100
+    console.log(result / people)
+    return result / people
+  }
   
   return (
     <>
@@ -9,24 +17,27 @@ function App() {
     <div>
       
       <h2>Bill</h2>
-      <input type="number" placeholder="0" />
+      <input 
+      type="number" 
+      value={bill} 
+      onChange={(e) => {setBill(e.target.value)}} />
 
       <div>
         <h2>Select tip %</h2>
         <article>
-          <button>5%</button>
+          <button onClick={() => handleClick(5)}>5%</button>
         </article>
         <article>
-          <button>10%</button>
+          <button onClick={() => handleClick(10)}>10%</button>
         </article>
         <article>
-          <button>15%</button>
+          <button onClick={() => handleClick(15)}>15%</button>
         </article>
         <article>
-          <button>25%</button>
+          <button onClick={() => handleClick(25)}>25%</button>
         </article>
         <article>
-          <button>50%</button>
+          <button onClick={() => handleClick(50)}>50%</button>
         </article>
         <article>
           <button>Custom</button>
@@ -34,8 +45,26 @@ function App() {
       </div>
 
       <h2>Number of People</h2>
-      <input type="text" />
+      <input
+      type="number" 
+      value={people} 
+      onChange={(e) => {setPeople(e.target.value)}} />
 
+      <article>
+        <div>
+          <h3>Tip Amount</h3>
+          <p>/ person</p>
+        </div>
+        <h2>$0.00</h2>
+      </article>
+
+      <article>
+        <div>
+          <h3>Total</h3>
+          <p>/ person</p>
+        </div>
+        <h2>$0.00</h2>
+      </article>
     </div>
     </>
     
